49fe9391310ff409a84d7ab3ab11acabe8154971
Davey Wright <Davey Wright davey.w.wright@gmail.com>
2018-01-05 12:26:14 +0000

    Organize stylesheet and refactor styles

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,7 +7,7 @@ class QuotesController < ApplicationController
 		@quote = Quote.create(quote_params)
 
 		if @quote.invalid?
-			flash[:error] = "<strong class='error h3'>SORRY!</strong> The data you entered is invalid and could not be saved."
+			flash[:error] = "<strong class='bold brand-color h3'>SORRY!</strong> The data you entered is invalid and could not be saved."
 		end
 
 		redirect_to root_path
